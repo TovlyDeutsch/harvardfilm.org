556b96cf4f5398e1d50feace8193fcb3555b150d
Tovly Deutsch <tovlydeutsch@gmail.com>
2018-08-14 00:37:48 -0400

    fixed react import path in whatwedo.jsx

--- src/components/WhatWeDo/WhatWeDo.jsx
+++ src/components/WhatWeDo/WhatWeDo.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "../../../node_modules/@types/react/index";
+import React, { Component } from "react";
 import camera from "../../picture.svg";
 import "./WhatWeDo.css";
 
