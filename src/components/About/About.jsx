a7df13980e683d80639a0c06c67c2cbf4487eaba
Tovly Deutsch <tovlydeutsch@gmail.com>
2018-08-13 23:59:07 -0400

    changed cover img path to local path

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,12 +6,34 @@ class About extends Component {
     return (
       <div className="hero-container">
         <figure className="hero-img">
-          <img width="100%" height="auto" src="https://www.hcs.harvard.edu/hufa/wp-content/uploads/2015/12/discover-love-film-1.jpg" className="vc_single_image-img attachment-full" alt="" />
+          <img
+            width="100%"
+            height="auto"
+            src="img/film-set.jpg"
+            className="vc_single_image-img attachment-full"
+            alt=""
+          />
         </figure>
         <div className="hero-text-wrapper">
           <h1 className="hero-statement">Film for all</h1>
-          <h5 className='intro-subheading'>HUFA is fostering filmmaking at Harvard and the wider Boston community.</h5>
-          <p className='hero-description'>HUFA, founded in 2017, is dedicated to facilitating video production at Harvard via club-wide film projects, educational workshops, feedback sessions among other programming. Early ideation for the club began in 2016 and we were approved as a provisional recognized student organization in the Fall of 2017. We are steadily entering <a href="https://www.youtube.com/watch?v=T3jlDUCIFtE">film competitions</a>&nbsp;and planning our first 48-hour Film Festival for the Arts First Weekend this Spring. We are continually seeking sponsorship to grow and support our up-and-coming club. If you’re a student, company, or anyone interested in the future of filmmaking, please get in touch!</p>
+          <h5 className="intro-subheading">
+            HUFA is fostering filmmaking at Harvard and the wider Boston
+            community.
+          </h5>
+          <p className="hero-description">
+            HUFA, founded in 2017, is dedicated to facilitating video production
+            at Harvard via club-wide film projects, educational workshops,
+            feedback sessions among other programming. Early ideation for the
+            club began in 2016 and we were approved as a provisional recognized
+            student organization in the Fall of 2017. We are steadily entering{" "}
+            <a href="https://www.youtube.com/watch?v=T3jlDUCIFtE">
+              film competitions
+            </a>&nbsp;and planning our first 48-hour Film Festival for the Arts
+            First Weekend this Spring. We are continually seeking sponsorship to
+            grow and support our up-and-coming club. If you’re a student,
+            company, or anyone interested in the future of filmmaking, please
+            get in touch!
+          </p>
           {/*
           <div className="view_more_button">
         	  <a className="view_more_link" href="http://www.hcs.harvard.edu/hufa/?page_id=1836" title="">VIEW MORE <i className="vc_btn3-icon fa fa-long-arrow-right"></i></a>
